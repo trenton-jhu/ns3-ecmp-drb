baed43e1bd8f31fa2a849ea8c527ac9541f883e1
trenton-uima <ywang382@jhu.edu>
2020-12-01 22:28:29 -0800

    update shell script to run both modes

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
-if test "$#" -ne 4; then
+if test "$#" -ne 3; then
     echo "Incorrect number of arguments"
-    echo "Usage: ./run.sh [number of trails] [ECMP/DRB] [flow size] [load]"
-    echo "Example: ./run.sh 5 ECMP 1000000 0.1"
+    echo "Usage: ./run.sh [number of trails] [flow size] [load]"
+    echo "Example: ./run.sh 5 1000000 0.1"
     exit 0
 fi
 
 rm -rf *.xml
-echo "Begin to run $1 trails with parameters --runMode=$2 --flowSize=$3 --load=$4"
+echo "Begin to run $1 trails with parameters --runMode=ECMP --flowSize=$2 --load=$3"
 for ((i = 1; i <= $1; i++)); do
     echo "Running trial $i ..."
-    ./waf --run "ecmp-drb --ID=$i --runMode=$2 --flowSize=$3 --load=$4" > /dev/null
+    ./waf --run "ecmp-drb --ID=$i --runMode=ECMP --flowSize=$2 --load=$3" > /dev/null
 done
-
+echo "============ ECMP Results ============="
 ./parse.py *.xml
 rm -rf *.xml
+
+echo "Begin to run $1 trails with parameters --runMode=DRB --flowSize=$2 --load=$3"
+for ((i = 1; i <= $1; i++)); do
+    echo "Running trial $i ..."
+    ./waf --run "ecmp-drb --ID=$i --runMode=DRB --flowSize=$2 --load=$3" > /dev/null
+done
+echo "============ DRB Results ============="
+./parse.py *.xml
+rm -rf *.xml
+
 exit 0
